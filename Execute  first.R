a5ce8b61c79b751cc78cbe04b7e3d4c273d951d2
JohnPickering <John.Pickering@otago.ac.nz>
2020-04-01 10:20:33 +1300

    New US state graph files. Updated Execute first.R and covid_fns.R

--- Execute  first.R
+++ Execute  first.R
@@ -1,5 +1,7 @@
 # COVID-19 Graphs for number of days needed to double
 
+# 01 April: Updated to extract US data separately
+
 library(tidyverse)
 library(ggrepel)
 library(lubridate)
@@ -7,11 +9,14 @@ library(rms)
 library(splines)
 library(readxl)
 source("covid_fns.R")
+
 # Load Data
 # Source: Johns Hopkins University Center for Systems Science and Engineering (JHU CSSE)
 confirmed_raw <- read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv")
 deaths_raw <- read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv")
 recovered_raw <- read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv")
+confirmed_US <- read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv")
+deaths_US <- read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_US.csv")
 
 # Confirmed
 confirmed_long_100 <- clean_jhd_to_long(confirmed_raw)
@@ -101,3 +106,66 @@ df_10 <- deaths_long_10 %>%
 
 save(df_10, file = "DEATHS Greater 10.Rdata")
 
+##########  US
+# Confirmed cases
+confirmed_long_100US <- clean_jhd_to_long_US(confirmed_US)
+confirmed_long_100US <- confirmed_long_100US %>% 
+  group_by(state) %>%
+  mutate(ndays = 1:n()) %>%  
+  ungroup() 
+confirmed_long_100US <- confirmed_long_100US %>% 
+  arrange(state, date) %>% 
+  group_by(state) %>%
+  mutate(change = ifelse(ndays == 1, 0, confirmed - lag(confirmed))) %>% 
+  ungroup() %>% 
+  filter(confirmed >= 100)  
+
+confirmed_long_100US <- confirmed_long_100US %>% 
+  group_by(state) %>%
+  mutate(ndays = 1:n()) %>%  
+  ungroup() 
+confirmed_long_100US <- confirmed_long_100US %>% 
+  mutate(change = ifelse(change < 5, NA, change)) %>% 
+  mutate(dbl = (confirmed - change)/change) %>% 
+  mutate(dbl = ifelse(is.infinite(dbl), NA, dbl)) 
+
+df_100US <- confirmed_long_100US %>% 
+  #left_join(states, by = "state") %>% 
+  mutate(ndays = ndays - 0.5) %>% # because when working out instantaneous slope is really the midpoint
+  group_by(state) %>% 
+  mutate(max_ndays = ifelse(ndays == max(ndays), max(ndays), NA)) %>% 
+  ungroup()  
+
+save(df_100US, file = "CASES US Greater 100.Rdata")
+
+
+# deaths 
+deaths_long_10US <- clean_jhd_to_long_US(deaths_US)
+deaths_long_10US <- deaths_long_10US %>% 
+  group_by(state) %>%
+  mutate(ndays = 1:n()) %>%  
+  ungroup() 
+deaths_long_10US <- deaths_long_10US %>% 
+  arrange(state, date) %>% 
+  group_by(state) %>%
+  mutate(change = ifelse(ndays == 1, 0, deaths - lag(deaths))) %>% 
+  ungroup() %>% 
+  filter(deaths >= 10)  
+
+deaths_long_10US <- deaths_long_10US %>% 
+  group_by(state) %>%
+  mutate(ndays = 1:n()) %>%  
+  ungroup() 
+deaths_long_10US <- deaths_long_10US %>% 
+  mutate(change = ifelse(change < 1, NA, change)) %>%   # Use <1 to avoid infinities
+  mutate(dbl = (deaths - change)/change) %>% 
+  mutate(dbl = ifelse(is.infinite(dbl), NA, dbl)) 
+
+df_10US <- deaths_long_10US %>% 
+  #left_join(states, by = "state") %>% 
+  mutate(ndays = ndays - 0.5) %>% # because when working out instantaneous slope is really the midpoint
+  group_by(state) %>% 
+  mutate(max_ndays = ifelse(ndays == max(ndays), max(ndays), NA)) %>% 
+  ungroup()  
+
+save(df_10US, file = "DEATHS US Greater 10.Rdata")
--- covid_fns.R
+++ covid_fns.R
@@ -1,5 +1,6 @@
 # Covid Functions
-# These functions are from https://joachim-gassen.github.io/2020/03/tidying-the-john-hopkins-covid-19-data/.  
+# These functions originated from from https://joachim-gassen.github.io/2020/03/tidying-the-john-hopkins-covid-19-data/.  
+# 1 April additional function to enable US state data to be graphed
 
 clean_jhd_to_long <- function(df) {
   df_str <- deparse(substitute(df))
@@ -23,6 +24,29 @@ clean_jhd_to_long <- function(df) {
 
 #########
 
+clean_jhd_to_long_US <- function(df) {
+  df_str <- deparse(substitute(df))
+  var_str <- substr(df_str, 1, str_length(df_str) - 3)
+  ifelse(df_str == "confirmed_US",
+         {df <- df %>% select(-`Country_Region`, -Lat, -`Long_`,  -Admin2, -UID, -iso2, -iso3, -code3, -FIPS, -Combined_Key) },
+         {df <- df %>% select(-`Country_Region`, -Lat, -`Long_`, -Population, -Admin2, -UID, -iso2, -iso3, -code3, -FIPS, -Combined_Key)}
+  )
+  df %>% group_by(`Province_State`) %>%
+    filter(`Province_State` != "Cruise Ship") %>%
+    mutate_at(vars(-group_cols()), sum) %>% 
+    distinct() %>%
+    ungroup() %>%
+    rename(state = `Province_State`) %>%
+    pivot_longer(
+      -state, 
+      names_to = "date_str",
+      values_to = var_str
+    ) %>%
+    mutate(date = mdy(date_str)) %>%
+    select(state, date, !! sym(var_str)) 
+}
+
+#########
 extract_country <- function(my_country, df) {
   country_change <- df %>% 
     filter(country == my_country) %>% 
